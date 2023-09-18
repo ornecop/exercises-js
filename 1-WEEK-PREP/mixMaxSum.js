0271d36688cfd19be53ab3a3d0d6ed31ff526705
Ornella <orne.el38@gmail.com>
2023-09-18 11:33:47 -0300

    first

---
+++ 1-WEEK-PREP/mixMaxSum.js
@@ -0,0 +1,45 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+arr.sort((a, b) => a - b);
+
+    const minSum = arr.slice(0, 4).reduce((acc, num) => acc + num, 0);
+
+    const maxSum = arr.slice(1, 5).reduce((acc, num) => acc + num, 0);
+
+    console.log(minSum, maxSum);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
